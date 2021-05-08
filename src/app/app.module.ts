601a34260274d17d61a77912669364f94904f374
David Marquillas <dmarquillas@gmail.com>
2021-05-08 18:14:18 -0300

    avences

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,20 @@ import { HomeComponent } from './components/home/home.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
-
+import { AppRoutingModule } from './app-routing.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SliderComponent } from './components/slider/slider.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, SliderComponent],
   imports: [
     BrowserModule,
     FlexLayoutModule,
     BrowserAnimationsModule,
     MatIconModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
